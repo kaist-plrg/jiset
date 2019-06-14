7d350a50d8ffbfbcd3c7dbda3e8b033a99826b12
jhnaldo <jhpjhp0223@gmail.com>
2019-06-14 11:19:25 +0900

    Fixed error in ToNumber

--- src/main/scala/kr/ac/kaist/ase/manualModel/algorithm/ToNumber.scala
+++ src/main/scala/kr/ac/kaist/ase/manualModel/algorithm/ToNumber.scala
@@ -3,12 +3,11 @@ package kr.ac.kaist.ase.manualModel
 import kr.ac.kaist.ase.core.Parser._
 import kr.ac.kaist.ase.core._
 object ToNumber {
-  val func: Func = Func(List(Id("argument")), parseInst(
+  val func: Func = Func("ToNumber", List(Id("argument")), parseInst(
     s"""if (= (typeof argument) "Number") {
           return argument
         } else {
           return undefined
-        }
-       """
+        }"""
   ))
 }
