e63432d6c10bd14ae9ec5c9e4fca9358563cfa7a
Park jihee <j31d0@kaist.ac.kr>
2019-07-08 17:16:35 +0900

    add filter with standard features

--- src/main/scala/kr/ac/kaist/ase/phase/FilterMeta.scala
+++ src/main/scala/kr/ac/kaist/ase/phase/FilterMeta.scala
@@ -14,6 +14,83 @@ import kr.ac.kaist.ase.util.TestConfigJsonProtocol._
 case object FilterMeta extends PhaseObj[Unit, FilterMetaConfig, Unit] {
   val name = "extract-meta"
   val help = "Extracts metadata"
+  val standardFeatures = List(
+    "ArrayBuffer",
+    "Array.prototype.values",
+    "arrow-function",
+    "async-iteration",
+    "async-functions",
+    "Atomics",
+    "caller",
+    "class",
+    "computed-property-names",
+    "const",
+    "cross-realm",
+    "DataView",
+    "DataView.prototype.getFloat32",
+    "DataView.prototype.getFloat64",
+    "DataView.prototype.getInt16",
+    "DataView.prototype.getInt32",
+    "DataView.prototype.getInt8",
+    "DataView.prototype.getUint16",
+    "DataView.prototype.getUint32",
+    "DataView.prototype.setUint8",
+    "default-parameters",
+    "destructuring-assignment",
+    "destructuring-binding",
+    "for-of",
+    "Float32Array",
+    "Float64Array",
+    "generators",
+    "Int8Array",
+    "Int32Array",
+    "json-superset",
+    "let",
+    "Map",
+    "new.target",
+    "object-rest",
+    "object-spread",
+    "Object.is",
+    "optional-catch-binding",
+    "Promise.prototype.finally",
+    "Proxy",
+    "Reflect",
+    "Reflect.construct",
+    "Reflect.set",
+    "Reflect.setPrototypeOf",
+    "regexp-dotall",
+    "regexp-lookbehind",
+    "regexp-named-groups",
+    "regexp-unicode-property-escapes",
+    "Set",
+    "SharedArrayBuffer",
+    "String.fromCodePoint",
+    "String.prototype.endsWith",
+    "String.prototype.includes",
+    "super",
+    "Symbol",
+    "Symbol.asyncIterator",
+    "Symbol.hasInstance",
+    "Symbol.isConcatSpreadable",
+    "Symbol.iterator",
+    "Symbol.match",
+    "Symbol.replace",
+    "Symbol.search",
+    "Symbol.species",
+    "Symbol.split",
+    "Symbol.toPrimitive",
+    "Symbol.toStringTag",
+    "Symbol.unscopables",
+    "tail-call-optimization",
+    "template",
+    "TypedArray",
+    "u180e",
+    "Uint8Array",
+    "Uint16Array",
+    "Uint8ClampedArray",
+    "WeakMap",
+    "WeakSet"
+  )
 
   def apply(
     unit: Unit,
@@ -46,7 +123,8 @@ case object FilterMeta extends PhaseObj[Unit, FilterMetaConfig, Unit] {
   ) &&
     (meta.locales.isEmpty) &&
     ((meta.name startsWith "/test/language/") ||
-      (meta.name startsWith "/test/built-ins/"))
+      (meta.name startsWith "/test/built-ins/")) &&
+      (meta.features.forall(standardFeatures contains _))
   def defaultConfig: FilterMetaConfig = FilterMetaConfig()
   val options: List[PhaseOption[FilterMetaConfig]] = List()
 }
