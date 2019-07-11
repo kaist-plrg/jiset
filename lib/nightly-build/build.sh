2d79e8834ac2c76a1c2157c514b1e921becf9052
Jihee Park <j31d0@users.noreply.github.com>
2019-07-11 12:58:32 +0900

    change build script to log error list

--- lib/nightly-build/build.sh
+++ lib/nightly-build/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # run at crontab. ex) 0 0 * * * /home/j31d0/ase/lib/nightly-build/build.sh
-logNAME=/home/j31d0/ase/$(date +"%Y%m%d%H%M%S").log
-export ASE_HOME=/home/j31d0/ase/
-(cd /home/j31d0/ase/ && git pull && sbt clean generateModel test262Test) 2> $logNAME
+BASE=/home/j31d0/ase/
+logNAME=$BASE/$(date +"%Y%m%d%H%M%S").log
+export ASE_HOME=$BASE
+(cd $BASE && git pull && sbt clean generateModel test262Test) 2> $logNAME
+sed "/Run completed in/q" $BASE/tests/detail | grep -A1 --no-group-separator "\*\*\* FAILED \*\*\*" | grep -v "\*\*\* FAILED \*\*\*" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g" | sort | uniq -c | sort -rg -k 1 > $BASE/tests/result/errlist_$(date +"%Y%m%d").log
 [ -s $logNAME ] || rm $logNAME
