e9643e117494f2269deb75b7aee90fd69e090a27
naldo <jhpjhp0223@gmail.com>
2019-07-13 14:37:18 +0900

    Fixed error in ToNumber/ToString

--- src/main/resources/es2018/manual/algorithm/ToNumber.scala
+++ src/main/resources/es2018/manual/algorithm/ToNumber.scala
@@ -6,22 +6,33 @@ object ToNumber {
   val func: Func = Func("ToNumber", List(Id("argument")), None, parseInst(
     s"""{
       let atype = (typeof argument)
-      if (= atype "Undefined") {
-        return NaN
-      } else if (= atype "Null") {
-        return 0
-      } else if (= atype "Boolean") {
-        if argument return 1 else return 0
-      } else if (= atype "Number") {
-        return argument
-      } else if (= atype "String") {
-        return (convert argument str2num)
-      } else if (= atype "Symbol") {
-        return NaN
-      } else if (= atype "Object") {
-        return NaN
-      } else {
-        ??? "Wrong type of argument for ToNumber"
+      if (= atype "Undefined") return NaN
+      else if (= atype "Null") return 0
+      else if (= atype "Boolean") if argument return 1 else return 0
+      else if (= atype "Number") return argument
+      else if (= atype "String") return (convert argument str2num)
+      else if (= atype "Symbol") return (new Completion (
+        "Type" -> CONST_throw,
+        "Value" -> (new OrdinaryObject(
+          "Prototype" -> INTRINSIC_TypeErrorPrototype,
+          "ErrorData" -> undefined,
+          "SubMap" -> (new SubMap())
+        )),
+        "Target" -> CONST_empty
+      ))
+      else {
+        let __x0__ = (ToPrimitive argument "Number")
+        if (= (typeof __x0__) "Completion") {
+          if (= __x0__.Type CONST_normal) __x0__ = __x0__.Value
+          else return __x0__
+        } else {}
+        let primValue = __x0__
+        let __x1__ = (ToNumber primValue)
+        if (= (typeof __x1__) "Completion") {
+          if (= __x1__.Type CONST_normal) __x1__ = __x1__.Value
+          else return __x1__
+        } else {}
+        return __x1__
       }
     }"""
   ))
--- src/main/resources/es2018/manual/algorithm/ToString.scala
+++ src/main/resources/es2018/manual/algorithm/ToString.scala
@@ -6,23 +6,33 @@ object ToString {
   val func: Func = Func("ToString", List(Id("argument")), None, parseInst(
     s"""{
       let atype = (typeof argument)
-      if (= atype "Undefined") {
-        return "undefined"
-      } else if (= atype "Null") {
-        return "null"
-      } else if (= atype "Boolean") {
-        if argument return "true"
-        else return "false"
-      } else if (= atype "Number") {
-        return (NumberToString argument)
-      } else if (= atype "String") {
-        return argument
-      } else if (= atype "Symbol") {
-        return (ThrowCompletion (new TypeError()))
-      } else if (= atype "Object") {
-        return (ToString (ToPrimitive argument "string"))
-      } else {
-        return undefined
+      if (= atype "Undefined") return "undefined"
+      else if (= atype "Null") return "null"
+      else if (= atype "Boolean") if argument return "true" else return "false"
+      else if (= atype "Number") return (NumberToString argument)
+      else if (= atype "String") return argument
+      else if (= atype "Symbol")  return (new Completion (
+        "Type" -> CONST_throw,
+        "Value" -> (new OrdinaryObject(
+          "Prototype" -> INTRINSIC_TypeErrorPrototype,
+          "ErrorData" -> undefined,
+          "SubMap" -> (new SubMap())
+        )),
+        "Target" -> CONST_empty
+      ))
+      else {
+        let __x0__ = (ToPrimitive argument "String")
+        if (= (typeof __x0__) "Completion") {
+          if (= __x0__.Type CONST_normal) __x0__ = __x0__.Value
+          else return __x0__
+        } else {}
+        let primValue = __x0__
+        let __x1__ = (ToString primValue)
+        if (= (typeof __x1__) "Completion") {
+          if (= __x1__.Type CONST_normal) __x1__ = __x1__.Value
+          else return __x1__
+        } else {}
+        return __x1__
       }
     }"""
   ))
