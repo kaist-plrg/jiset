84860990b26d2df8a691c3d483c0d6852e7c2289
naldo <jhpjhp0223@gmail.com>
2019-08-20 20:43:31 +0900

    Updated images

--- icse/img/all-version-sem-delta.R
+++ icse/img/all-version-sem-delta.R
@@ -7,19 +7,19 @@ data <- data.frame(ECMAScript_Version, auto, manual)
 
 p <- plot_ly(
   data,
-  y = ~ECMAScript_Version,
-  x = ~auto,
+  x = ~ECMAScript_Version,
+  y = ~auto,
   type = 'bar',
   name = 'auto',
-  marker = list(color = '#0070DE'),
-  orientation = 'h'
+  marker = list(color = '#0070DE')
+  # orientation = 'h'
 ) %>% add_trace(
-  x = ~manual,
+  y = ~manual,
   name = 'manual',
   marker = list(color = '#FE1A13')
 ) %>% layout(
-  xaxis = list(title = '# of Steps'),
-  yaxis = list(title = '', showticklabels = FALSE),
+  yaxis = list(title = '# of Steps'),
+  xaxis = list(title = ''), #, showticklabels = FALSE),
   barmode = 'stack'
 )
 p
