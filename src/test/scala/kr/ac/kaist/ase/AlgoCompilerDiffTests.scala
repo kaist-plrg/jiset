d881054ce2e9c0d2ad34790bcaa366ce6a0cfde2
jhnaldo <jhpjhp0223@gmail.com>
2019-08-23 16:45:00 +0900

    Show language/builtin for algorithm diff tests

--- src/test/scala/kr/ac/kaist/ase/AlgoCompilerDiffTests.scala
+++ src/test/scala/kr/ac/kaist/ase/AlgoCompilerDiffTests.scala
@@ -60,74 +60,80 @@ class AlgoCompilerDiffTest extends CoreTest {
 
   // registration
   def init: Unit = {
-    var algoMap: Map[String, List[List[Token]]] = Map()
-    var algoMap2: Map[String, List[List[Token]]] = Map()
-    var firstStepMap: Map[String, Boolean] = Map()
-    var firstAlgoMap: Map[String, Boolean] = Map()
+    def show(name: String, filter: Algorithm => Boolean): Unit = {
+      println(s"========================================")
+      println(name)
+      println(s"========================================")
+      var algoMap: Map[String, List[List[Token]]] = Map()
+      var algoMap2: Map[String, List[List[Token]]] = Map()
+      var firstStepMap: Map[String, Boolean] = Map()
+      var firstAlgoMap: Map[String, Boolean] = Map()
 
-    var nextStepMap: Map[String, Boolean] = Map()
-    var nextAlgoMap: Map[String, Boolean] = Map()
+      var nextStepMap: Map[String, Boolean] = Map()
+      var nextAlgoMap: Map[String, Boolean] = Map()
 
-    var diffStepMap: Map[String, Boolean] = Map()
-    var diffAlgoMap: Map[String, Boolean] = Map()
+      var diffStepMap: Map[String, Boolean] = Map()
+      var diffAlgoMap: Map[String, Boolean] = Map()
 
-    var (apass, atotal, dapass, datotal, spass, stotal, dspass, dstotal) = (0, 0, 0, 0, 0, 0, 0, 0)
+      var (apass, atotal, dapass, datotal, spass, stotal, dspass, dstotal) = (0, 0, 0, 0, 0, 0, 0, 0)
 
-    DIFFLIST.foreach((version) => {
-      val algoversionDir = s"$RESOURCE_DIR/$version/auto/algorithm"
-      algoMap2 = Map()
-      nextStepMap = Map()
-      nextAlgoMap = Map()
-      diffStepMap = Map()
-      diffAlgoMap = Map()
+      DIFFLIST.foreach((version) => {
+        val algoversionDir = s"$RESOURCE_DIR/$version/auto/algorithm"
+        algoMap2 = Map()
+        nextStepMap = Map()
+        nextAlgoMap = Map()
+        diffStepMap = Map()
+        diffAlgoMap = Map()
 
-      for (file <- shuffle(walkTree(new File(algoversionDir)))) {
-        val filename = file.getName
-        if (jsonFilter(filename)) {
-          val name = file.toString
-          val algo = Algorithm(name)
-          // if (algo.kind == Builtin) {
-          // if (algo.kind == Language) {
-          algoMap2 += algo.filename.split("/").last -> splitAlgo(algo)
-          val diffStepSet = algoMap.get(algo.filename.split("/").last).map((x) => findDiffStepSet(x, splitAlgo(algo))).getOrElse((0 until algo.lineCount).toList)
-          val isDiff = !(diffStepSet.isEmpty)
-          val lineCount = algo.lineCount
-          lazy val compiler = GeneralAlgoCompiler("", algo)
-          lazy val (func, failed) = compiler.result
-          nextAlgoMap += name -> (failed.size == 0)
-          if (isDiff) diffAlgoMap += name -> (failed.size == 0)
-          (0 until lineCount).foreach((k) => {
-            nextStepMap += s"$name$k" -> !(failed contains k)
-            if (diffStepSet contains k) diffStepMap += s"$name$k" -> !(failed contains k)
-          })
+        for (file <- shuffle(walkTree(new File(algoversionDir)))) {
+          val filename = file.getName
+          if (jsonFilter(filename)) {
+            val name = file.toString
+            val algo = Algorithm(name)
+            if (filter(algo)) {
+              algoMap2 += algo.filename.split("/").last -> splitAlgo(algo)
+              val diffStepSet = algoMap.get(algo.filename.split("/").last).map((x) => findDiffStepSet(x, splitAlgo(algo))).getOrElse((0 until algo.lineCount).toList)
+              val isDiff = !(diffStepSet.isEmpty)
+              val lineCount = algo.lineCount
+              lazy val compiler = GeneralAlgoCompiler("", algo)
+              lazy val (func, failed) = compiler.result
+              nextAlgoMap += name -> (failed.size == 0)
+              if (isDiff) diffAlgoMap += name -> (failed.size == 0)
+              (0 until lineCount).foreach((k) => {
+                nextStepMap += s"$name$k" -> !(failed contains k)
+                if (diffStepSet contains k) diffStepMap += s"$name$k" -> !(failed contains k)
+              })
+            }
+          }
         }
-      }
-      algoMap = algoMap2
-      firstStepMap = nextStepMap
-      firstAlgoMap = nextAlgoMap
+        algoMap = algoMap2
+        firstStepMap = nextStepMap
+        firstAlgoMap = nextAlgoMap
 
-      val (ap, at) = countPass(nextAlgoMap)
-      val (dap, dat) = countPass(diffAlgoMap)
-      val (sp, st) = countPass(nextStepMap)
-      val (dsp, dst) = countPass(diffStepMap)
+        val (ap, at) = countPass(nextAlgoMap)
+        val (dap, dat) = countPass(diffAlgoMap)
+        val (sp, st) = countPass(nextStepMap)
+        val (dsp, dst) = countPass(diffStepMap)
 
-      apass += ap; atotal += at
-      spass += sp; stotal += st
-      if (version != "es2016") {
-        dapass += dap; datotal += dat
-        dspass += dsp; dstotal += dst
-      }
+        apass += ap; atotal += at
+        spass += sp; stotal += st
+        if (version != "es2016") {
+          dapass += dap; datotal += dat
+          dspass += dsp; dstotal += dst
+        }
 
-      println(s"$version algo: ${getCountString(ap, at)}")
-      println(s"     Δ algo: ${getCountString(dap, dat)}")
-      println(s"$version step: ${getCountString(sp, st)}")
-      println(s"     Δ step: ${getCountString(dsp, dst)}")
-      println(s"----------------------------------------")
-    })
-    println(s"  algo: ${getCountString(apass, atotal)}")
-    println(s"Δ algo: ${getCountString(dapass, datotal)}")
-    println(s"  step: ${getCountString(spass, stotal)}")
-    println(s"Δ step: ${getCountString(dspass, dstotal)}")
+        println(s"$version step: ${getCountString(sp, st)}")
+        println(s"     Δ step: ${getCountString(dsp, dst)}")
+        println(s"----------------------------------------")
+      })
+      println(s"  step: ${getCountString(spass, stotal)}")
+      println(s"Δ step: ${getCountString(dspass, dstotal)}")
+      println(s"========================================")
+      println
+    }
+    show("All", x => true)
+    show("Language", _.kind == Language)
+    show("Builtin", _.kind == Builtin)
   }
   init
 }
