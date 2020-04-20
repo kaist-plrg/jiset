9b8636f94bb00c47c09668e467cdf4fe6b6ec659
Jihee Park <j31d0@users.noreply.github.com>
2020-04-20 12:34:35 +0900

    More verbose error function

--- src/main/resources/es2019_full/manual/algorithm/HostReportErrors.scala
+++ src/main/resources/es2019_full/manual/algorithm/HostReportErrors.scala
@@ -1,5 +1,18 @@
 object HostReportErrors {
   val func: Func = parseFunc(""""HostReportErrors" (errorList) => {
+    if (< 0 errorList.length) {
+      if (= undefined errorList[0i].ErrorData) {
+        if (= absent errorList[0i].Prototype) {} else {
+          if (= absent errorList[0i].Prototype.SubMap) {} else {
+            if (= absent errorList[0i].Prototype.SubMap.name) {} else {
+              assert errorList[0i].Prototype.SubMap.name.Value
+            }
+          }
+        }
+      } else {
+        assert errorList[0i].ErrorData
+      }
+    } else {}
     assert "Error occured"
     return (new Completion("Type" -> CONST_normal, "Value" -> undefined, "Target" -> CONST_empty))
   }""")
