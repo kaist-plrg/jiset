c9e994b259c807e145a8f91e1cf5a38ebf430d87
jhnaldo <jhpjhp0223@gmail.com>
2020-04-23 16:13:53 +0900

    Updated algo-step-diff.sh & grammar-diff.sh

--- prop-result/grammar-diff.sh
+++ prop-result/grammar-diff.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# prop_list="prop_matchAll prop_globalThis prop_nulllish_coalescing prop_Promise_allSettled prop_optional_chaining prop_BigInt"
-prop_list="prop_BigInt"
+prop_list="\
+  prop_matchAll \
+  prop_globalThis \
+  prop_nulllish_coalescing \
+  prop_Promise_allSettled \
+  prop_optional_chaining \
+  prop_BigInt \
+  prop_dynamic_import \
+"
 
 for prop in $prop_list
 do
   git checkout origin/$prop
   git submodule update --recursive > /dev/null
-  echo "clean & compile..."
+  echo "[$prop] clean & compile..."
   sbt clean compile 2>&1 >> /dev/null
-  echo "completed!"
-  echo "run grammar-diff..."
+  echo "[$prop] completed!"
+  echo "[$prop] run grammar-diff..."
   sbt "run grammar-diff"
 done
 
