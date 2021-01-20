290caa60bd75b96bd62aac11b738bae40c8b60b2
jhnaldo <jhpjhp0223@gmail.com>
2021-01-20 14:50:49 +0900

    Import ir._ in ReferenceChecker

--- src/main/scala/kr/ac/kaist/jiset/checker/ReferenceChecker.scala
+++ src/main/scala/kr/ac/kaist/jiset/checker/ReferenceChecker.scala
@@ -2,7 +2,7 @@ package kr.ac.kaist.jiset.checker
 
 import kr.ac.kaist.jiset.spec._
 import kr.ac.kaist.jiset.spec.algorithm.Algo
-import kr.ac.kaist.ires.ir
+import kr.ac.kaist.ires.ir._
 
 object ReferenceChecker extends Checker {
   // for specifications
@@ -15,19 +15,19 @@ object ReferenceChecker extends Checker {
   def apply(initial: Set[String], algo: Algo): Option[ReferenceError] = {
     var defined: Set[String] = initial ++ algo.params.map(_.name).toSet
     var errors = Set[String]()
-    object Walker extends ir.UnitWalker {
-      override def walk(inst: ir.Inst): Unit = inst match {
-        case ir.ILet(x, e) =>
+    object Walker extends UnitWalker {
+      override def walk(inst: Inst): Unit = inst match {
+        case ILet(x, e) =>
           walk(e); defined += x.name
-        case ir.IApp(x, f, as) =>
-          walk(f); walkList[ir.Expr](as, walk); defined += x.name
-        case ir.IAccess(x, b, e) =>
+        case IApp(x, f, as) =>
+          walk(f); walkList[Expr](as, walk); defined += x.name
+        case IAccess(x, b, e) =>
           walk(b); walk(e); defined += x.name
-        case ir.IWithCont(x, ps, b) =>
-          walkList[ir.Id](ps, walk); walk(b); defined += x.name
+        case IWithCont(x, ps, b) =>
+          walkList[Id](ps, walk); walk(b); defined += x.name
         case _ => super.walk(inst)
       }
-      override def walk(id: ir.Id): Unit = {
+      override def walk(id: Id): Unit = {
         val name = id.name
         if (!defined.contains(name) && !name.startsWith("CONST_")) errors += name
       }
