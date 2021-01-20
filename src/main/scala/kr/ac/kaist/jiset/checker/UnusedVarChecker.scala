f48cb91419f8353c37ae96e61339a3c0a68572e2
Yusung Sim <yusungsim@kaist.ac.kr>
2021-01-20 19:29:41 +0900

    Change UnusedVarChecker, syntax-directed function's parameters are not reported as wargning

--- src/main/scala/kr/ac/kaist/jiset/checker/UnusedVarChecker.scala
+++ src/main/scala/kr/ac/kaist/jiset/checker/UnusedVarChecker.scala
@@ -1,8 +1,15 @@
 package kr.ac.kaist.jiset.checker
 
 import kr.ac.kaist.jiset.spec._
-import kr.ac.kaist.jiset.spec.algorithm.Algo
+import kr.ac.kaist.jiset.spec.algorithm.{ Algo, Head, SyntaxDirectedHead }
 import kr.ac.kaist.ires.ir._
+import org.jsoup.nodes.Document.OutputSettings.Syntax
+
+sealed trait VarStatus
+case object Unused extends VarStatus
+case object SyntaxUnused extends VarStatus
+case object ParamUnused extends VarStatus
+case object Used extends VarStatus
 
 object UnusedVarChecker extends Checker {
   // for specifications
@@ -13,28 +20,31 @@ object UnusedVarChecker extends Checker {
 
   // for algorithms
   def apply(initial: Set[String], algo: Algo): Option[UnusedVariable] = {
-    var scopeVars: Map[String, Boolean] = algo.params.map(_.name -> false).toMap
+    var scopeVars: Map[String, VarStatus] = algo.head match {
+      case _: SyntaxDirectedHead => algo.params.map(_.name -> SyntaxUnused).toMap
+      case _ => algo.params.map(_.name -> ParamUnused).toMap
+    }
     object Walker extends UnitWalker {
       override def walk(inst: Inst): Unit = inst match {
         case ILet(x, e) =>
-          walk(e); scopeVars += x.name -> false
+          walk(e); scopeVars += x.name -> Unused
         case IApp(x, f, as) =>
-          walk(f); walkList[Expr](as, walk); scopeVars += x.name -> false
+          walk(f); walkList[Expr](as, walk); scopeVars += x.name -> Unused
         case IAccess(x, b, e) =>
-          walk(b); walk(e); scopeVars += x.name -> false
+          walk(b); walk(e); scopeVars += x.name -> Unused
         case IWithCont(x, ps, b) =>
-          walkList[Id](ps, walk); walk(b); scopeVars += x.name -> false
+          walkList[Id](ps, walk); walk(b); scopeVars += x.name -> Unused
         case _ => super.walk(inst)
       }
       override def walk(id: Id): Unit = {
         val name = id.name
         if (scopeVars.keySet.exists(_ == name)) {
-          scopeVars += name -> true
+          scopeVars += name -> Used
         }
       }
     }
     Walker.walk(algo.body)
-    val errors = scopeVars.filter(t => !t._2).keySet
+    val errors = scopeVars.filter(t => t._2 != Used && t._2 != SyntaxUnused).keySet
     if (errors.isEmpty) None
     else Some(UnusedVariable(algo, errors))
   }
