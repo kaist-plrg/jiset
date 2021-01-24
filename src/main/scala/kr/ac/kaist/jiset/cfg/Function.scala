64384b68cf14bc763a011120c75bd4c0e8804560
jhnaldo <jhpjhp0223@gmail.com>
2021-01-24 15:42:05 +0900

    Beautified form of cfg/Function in DOT

---
+++ src/main/scala/kr/ac/kaist/jiset/cfg/Function.scala
@@ -0,0 +1,59 @@
+package kr.ac.kaist.jiset.cfg
+
+import kr.ac.kaist.ires.ir._
+import kr.ac.kaist.jiset.LINE_SEP
+import kr.ac.kaist.jiset.util.UId
+
+// CFG functions
+case class Function(
+    entry: Entry,
+    exit: Exit,
+    nodes: Set[Node],
+    forwards: Map[Node, (Edge, Node)]
+) extends UId {
+  // conversion to DOT
+  def toDot: String = {
+    val sb = new StringBuilder
+    def add(str: String): Unit = sb.append(str + LINE_SEP)
+    add(s"""digraph {""")
+    add(s"""  graph [fontname="consolas"]""")
+    add(s"""  node [fontname="consolas"]""")
+    add(s"""  edge [fontname="consolas"]""")
+    nodes.foreach(node => {
+      val uid = node.uid
+      node match {
+        case Entry() =>
+          add(s"""  node$uid [shape=point]""")
+        case Exit() =>
+          add(s"""  node$uid [shape=point]""")
+        case Block(insts) =>
+          add(s"""  node$uid [shape=none, margin=0, label=<""")
+          add(s"""    <table border="0" cellborder="1" cellspacing="0" cellpadding="10">""")
+          insts.foreach(inst => {
+            add(s"""      <tr><td align="left">${norm(inst)}</td></tr>""")
+          })
+          add(s"""    </table>""")
+          add(s"""  >]""")
+        case Call(inst) =>
+          add(s"""  node$uid [shape=hexagon, label=<${norm(inst)}>]""")
+        case Branch(cond) =>
+          add(s"""  node$uid [shape=diamond, label="${norm(cond)}"]""")
+      }
+    })
+    forwards.foreach(_ match {
+      case (from, (edge, to)) =>
+        val opt = edge match {
+          case NormalEdge => ""
+          case CondEdge(pass) => s""" [label="$pass"]"""
+        }
+        add(s"""  node${from.uid} -> node${to.uid}$opt""")
+    })
+    add(s"""}""")
+    sb.toString
+  }
+
+  // normalize beautified ires nodes
+  def norm(node: IRNode): String = {
+    beautify(node).replaceAll(">", "&gt;")
+  }
+}
