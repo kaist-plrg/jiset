e1354e78ba651ade5b2cc5cc16eeed92e07a2b91
h2oche <h2oche22@gmail.com>
2021-02-17 19:29:41 +0900

    Related to #155: generalize condOpCond

--- src/main/scala/kr/ac/kaist/jiset/parser/algorithm/Compiler.scala
+++ src/main/scala/kr/ac/kaist/jiset/parser/algorithm/Compiler.scala
@@ -1178,12 +1178,35 @@ object Compiler extends Compilers {
   )
 
   // conditional operators
-  lazy val condOpCond: P[I[Expr]] = _cond ~ (condOp <~ opt("if")) ~ _cond ^^ {
-    case (i ~ l) ~ ((op, _)) ~ (i1 ~ r) if isEmptyInsts(i1) => pair(i, EBOp(op, l, r))
-    case (i0 ~ l) ~ ((op, f)) ~ (i1 ~ r) =>
-      val temp = getTempId
-      val (t, e) = f(ISeq(i1 :+ IAssign(toRef(temp), r)))
-      pair(i0 ++ List(ILet(temp, l), IIf(toERef(temp), t, e)), toERef(temp))
+  lazy val _condOpCond = (
+    rhsCond |||
+    bopCond |||
+    containsCond |||
+    strictModeCond |||
+    sameCond |||
+    finiteCond |||
+    earlyErrorCond
+  )
+  lazy val condOpCond: P[I[Expr]] = rep1sep(_condOpCond, ",") ~ (condOp <~ opt("if")) ~ _cond ^^ {
+    case il ~ ((op, _)) ~ (i1 ~ r) if il.tail.forall { case i ~ e => isEmptyInsts(i) } && isEmptyInsts(i1) => {
+      val i ~ l = il.reduce[I[Expr]] {
+        case ((i0 ~ x), (i1 ~ y)) => pair(i0 ++ i1, EBOp(op, x, y))
+      }
+      pair(i ++ i1, EBOp(op, l, r))
+    }
+    case il ~ ((op, f)) ~ ie => {
+      var temp = getTempId
+      val inst = (il :+ ie).foldRight(emptyInst) {
+        case (i ~ expr, iacc) => {
+          val (t, e) = f(iacc)
+          val ifInst =
+            if (isEmptyInsts(List(t, e))) emptyInst
+            else IIf(toERef(temp), t, e)
+          ISeq(i :+ IAssign(toRef(temp), expr) :+ ifInst)
+        }
+      }
+      pair(List(ILet(temp, EBool(true)), inst), toERef(temp))
+    }
   }
   lazy val condOp: P[(BOp, Inst => (Inst, Inst))] = opt(",") ~> (
     "or" ^^^ { (OOr, (x: Inst) => (emptyInst, x)) } |||
