3d1bdc3dcc6ab63939cd5cf044c2bc6c929b2aae
Yusung Sim <yusungsim@kaist.ac.kr>
2021-03-09 15:05:45 +0900

    AnalyzerResultTest -> ManualTinyTest, comparison of result and answer

--- src/test/scala/kr/ac/kaist/jiset/analyzer/ManualTinyTest.scala
+++ src/test/scala/kr/ac/kaist/jiset/analyzer/ManualTinyTest.scala
@@ -4,24 +4,31 @@ import kr.ac.kaist.jiset.JISETTest
 import kr.ac.kaist.jiset.spec.ECMAScript
 import kr.ac.kaist.jiset.parser.ECMAScriptParser
 import kr.ac.kaist.jiset.cfg.{ CFG, Function }
-import kr.ac.kaist.jiset.util.Useful.{ error }
+import kr.ac.kaist.jiset.util.Useful._
 import kr.ac.kaist.jiset.analyzer.domain._
+import kr.ac.kaist.jiset.analyzer.domain.Beautifier._
 import kr.ac.kaist.jiset.ir._
+import org.scalatest._
 
-class AnalyzerResultTest extends AnalyzerTest {
-  val answerMap: Map[(String, View), (Option[AbsHeap], Option[AbsValue])] = Map(
-    ("ObjectEnvironmentRecord.HasThisBinding", View(List(NullT))) -> (None, Some(AbsPrim(Null)))
+class ManualTinyTest extends AnalyzerTest {
+  val answerMap: Map[(String, View), (AbsHeap, AbsValue)] = Map(
+    ("PrimaryExpression[0,0].IsIdentifierRef", View(List(AstT("PrimaryExpression"))))
+      -> ((AbsHeap.Bot, AbsComp(CompNormal -> (AF: AbsPure, emptyConst))))
   )
 
-  def compareAbsResult(result: (AbsHeap, AbsValue), answer: (Option[AbsHeap], Option[AbsValue])): Boolean = answer match {
-    case (Some(h), None) => result._1 == h
-    case (None, Some(v)) => result._2 == v
-    case (Some(h), Some(v)) => result._1 == h && result._2 == v
-    case (None, None) => true
+  def compareAbsResult(result: (AbsHeap, AbsValue), answer: (AbsHeap, AbsValue)): Boolean = {
+    val (resH, resV) = result
+    val (ansH, ansV) = answer
+    resH === ansH && resV === ansV
   }
 
   def getFunctionByName(cfg: CFG, fname: String): Option[Function] =
     cfg.algo2fid.get(fname).flatMap(uid => cfg.fidMap.get(uid))
+
+  def getString(h: AbsHeap, v: AbsValue): String = beautify(v) + (
+    if (h.isBottom) ""
+    else s" @ ${beautify(h)}"
+  )
 
   def init: Unit = {
     val pre = JISETTest.specInputs("recent")
@@ -33,16 +40,19 @@ class AnalyzerResultTest extends AnalyzerTest {
     // analyze results in sem: AbsSemantics
 
     // find testcases
-    check("Compare analyze results", for (((fname, view), answer) <- answerMap) {
+    check("Compare analyze results", for (((fname, view), (ansH, ansV)) <- answerMap) {
       val func = getFunctionByName(cfg, fname).getOrElse(error("Answer Function not found"))
       val rp = ReturnPoint(func, view)
-      val result = sem(rp)
-      val comparison = compareAbsResult(result, answer)
+      val (resH, resV) = sem(rp)
+      val comparison = resH === ansH && resV === ansV
       if (!comparison) {
         println(s"FAILED: Function $fname, View $view")
+        println(s"expected: ${getString(ansH, ansV)}")
+        println(s"result: ${getString(resH, resV)}")
         assert(comparison)
       }
     })
   }
+  init
 }
 
