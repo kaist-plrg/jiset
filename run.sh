61c467bdd7b82320d1516b56cfa680b6155f7e0c
jhnaldo <jhpjhp0223@gmail.com>
2021-03-18 22:08:55 +0900

    Updated run.sh

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ mkdir -p $prev
 rm -f $log
 
 echo "update..."
-git pull 2>&1 >> $log
-git submodule update 2>&1 >> $log
+git pull 2>> $log 1>> $log
+git submodule update 2>> $log 1>> $log
 echo "build project..."
-sbt assembly 2>&1 >> $log
+sbt assembly 2>> $log 1>> $log
 echo "run analyze..."
-jiset analyze -log -analyze:target=.* 2> /dev/null >> $log
+jiset analyze -log -analyze:target=.* 2>> $log 1>> $log
 mv $prev $filename
 if [ -f "cfg.pdf" ]; then
   mv cfg.dot cfg_trans.dot cfg.pdf $filename
