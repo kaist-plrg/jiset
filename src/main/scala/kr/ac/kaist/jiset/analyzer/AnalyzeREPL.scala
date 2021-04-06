ea6133e0bd9dade5f35e18f48fee3f9d803c5f7b
Wonho Shin <wonhoshin@iMac-4.local>
2021-04-06 13:44:26 +0900

    Fixed bug and Revert c7a9723022a74a37e9d622465f7310c93afce82e

--- src/main/scala/kr/ac/kaist/jiset/analyzer/AnalyzeREPL.scala
+++ src/main/scala/kr/ac/kaist/jiset/analyzer/AnalyzeREPL.scala
@@ -21,23 +21,24 @@ object AnalyzeREPL {
 
   // breakpoints
   private var continue = false
-  private val breakpoints = ArrayBuffer[(CmdOption, String)]()
+  private val breakpoints = ArrayBuffer[(String, String)]()
 
   // completer
   private val completer: TreeCompleter =
     new TreeCompleter(Command.commands.map(optionNode(_)): _*)
   private def optionNode(cmd: Command) =
     node(cmd.name :: (cmd match {
-      case CmdGraph => node("-total") :: cfg.funcs.map(x => node(x.name))
+      case CmdGraph =>
+        node(s"-${CmdGraph.total}") :: cfg.funcs.map(x => node(x.name))
       case _ => cmd.options.map(argNode(_))
     }): _*)
-  private def argNode(opt: CmdOption) =
-    node(s"-${opt.name}" :: getArgNodes(opt): _*)
-  private def getArgNodes(opt: CmdOption): List[TreeCompleter.Node] = opt match {
-    case CmdBreak.FuncTarget => cfg.funcs.map(x => node(x.name))
-    case CmdBreak.BlockTarget => (0 until cfg.nidGen.size).map(x => node(x.toString)).toList
-    case CmdInfo.RetTarget => cfg.funcs.map(x => node(x.name))
-    case CmdInfo.BlockTarget => (0 until cfg.nidGen.size).map(x => node(x.toString)).toList
+  private def argNode(opt: String) =
+    node(s"-$opt" :: getArgNodes(opt): _*)
+  private def getArgNodes(opt: String): List[TreeCompleter.Node] = opt match {
+    case CmdBreak.func => cfg.funcs.map(x => node(x.name))
+    case CmdBreak.block => (0 until cfg.nidGen.size).map(x => node(x.toString)).toList
+    case CmdInfo.ret => cfg.funcs.map(x => node(x.name))
+    case CmdInfo.block => (0 until cfg.nidGen.size).map(x => node(x.toString)).toList
     case _ => Nil
   }
 
@@ -49,78 +50,56 @@ object AnalyzeREPL {
     .build()
   private val prompt: String = LINE_SEP + s"${MAGENTA}analyzer>${RESET} "
 
-  // helper for break
-  private def isBreak(cp: ControlPoint): Boolean = cp match {
-    case NodePoint(node: Entry, _) => breakpoints.exists {
-      case (CmdBreak.FuncTarget, name) => name == cfg.funcOf(node).name
-      case (CmdBreak.BlockTarget, uid) => uid.toInt == node.uid
-      case _ => ???
-    }
-    case NodePoint(node, _) => breakpoints.exists {
-      case (CmdBreak.BlockTarget, uid) => uid.toInt == node.uid
-      case _ => false
-    }
-    case _ => false
-  }
-  private def addBreak(opt: CmdOption, bp: List[String]): Unit = bp match {
-    case Nil => println("need arguments")
-    case str :: _ => breakpoints += (opt -> str)
-  }
-
-  // stop
-  private def stop(): Unit = { breakpoints.clear(); continue = true }
-
   // help
   private lazy val help = { Command.help; println }
 
-  // info
-  private def printInfo(opt: CmdOption, args: List[String]): Unit = args match {
-    case Nil => println("need arguments")
-    case str :: _ =>
-      val info = opt match {
-        case CmdInfo.RetTarget => sem.getReturnPointByName(str)
-        case CmdInfo.BlockTarget => sem.getNodePointsById(str.toInt)
-      }
-      info.foreach(cp => {
-        println(sem.getString(cp, CYAN, true))
-        println
-      })
+  // break
+  private def break(args: List[String]) = {
+    import CmdBreak._
+    args match {
+      case opt :: bp :: _ if options contains opt.substring(1) =>
+        breakpoints += (opt.substring(1) -> bp)
+      case _ => println("Inappropriate argument")
+    }
   }
 
-  // entry
-  var visited: Set[ReturnPoint] = Set()
-
-  private def getEntryFunc(cp: ControlPoint): Set[String] = {
-    val rp = getRpOf(cp)
-    if (visited contains rp) Set()
-    else {
-      visited += rp
-      rp.func.algo.head match {
-        case head @ SyntaxDirectedHead(_, _, _, _, _, withParam) if withParam.isEmpty =>
-          Set(rp.func.name)
-        case _ => getCallNodes(rp).flatMap(getEntryFunc(_))
+  // rm-break
+  private def rmBreak(args: List[String]) = {
+    import CmdRmBreak._;
+    args match {
+      case Nil => println("need arguments")
+      case arg :: _ => optional(arg.toInt) match {
+        case _ if arg == s"-$all" => breakpoints.clear
+        case Some(idx) if idx.toInt < breakpoints.size =>
+          breakpoints.remove(idx.toInt)
+        case _ => println("Inappropriate argument")
       }
     }
   }
 
-  private def getRpOf(cp: ControlPoint): ReturnPoint = cp match {
-    case rp @ ReturnPoint(_, _) => rp
-    case NodePoint(node, view) =>
-      cfg.funcs.find(x => x.nodes.exists(x => x.uid == node.uid)) match {
-        case None => ???
-        case Some(f) => ReturnPoint(f, view)
-      }
+  // break heler
+  private def isBreak(cp: ControlPoint): Boolean = cp match {
+    case NodePoint(node: Entry, _) => breakpoints.exists {
+      case (s"${ CmdBreak.func }", name) => name == cfg.funcOf(node).name
+      case (s"${ CmdBreak.block }", uid) => uid.toInt == node.uid
+      case _ => ???
+    }
+    case NodePoint(node, _) => breakpoints.exists {
+      case (s"${ CmdBreak.block }", uid) => uid.toInt == node.uid
+      case _ => false
+    }
   }
 
-  private def getCallNodes(rp: ReturnPoint): Set[NodePoint[Call]] =
-    sem.getRetEdges(rp).map(_._1)
-
-  private def graph(cp: Option[ControlPoint], args: List[String]) =
+  // graph
+  private def graph(cp: Option[ControlPoint], args: List[String]) = {
+    import CmdGraph._
     optional(args.head.toInt) match {
       case Some(depth) => dumpCFG(cp, depth = Some(depth))
       case None if args.isEmpty => dumpCFG(cp, depth = Some(0))
+      case None if args.head == s"-$total" => dumpCFG(cp, depth = None)
       case None => graphFunc(args.head, args.tail)
     }
+  }
   private def graphFunc(
     fname: String,
     tail: List[String]
@@ -143,6 +122,57 @@ object AnalyzeREPL {
       }
   }
 
+  // stop
+  private def stop(): Unit = { breakpoints.clear(); continue = true }
+
+  // info
+  private def info(args: List[String]) = {
+    import CmdInfo._
+    args match {
+      case opt :: target :: _ if options contains opt.substring(1) =>
+        printInfo(opt.substring(1), target)
+      case _ => println("Inappropriate option")
+    }
+  }
+  private def printInfo(opt: String, target: String): Unit = {
+    import CmdInfo._
+    val info = opt match {
+      case CmdInfo.ret => sem.getReturnPointByName(target)
+      case CmdInfo.block if optional(target.toInt) != None =>
+        sem.getNodePointsById(target.toInt)
+      case _ => println("Inappropriate argument"); Set()
+    }
+    info.foreach(cp => {
+      println(sem.getString(cp, CYAN, true))
+      println
+    })
+  }
+
+  // entry
+  var visited: Set[ReturnPoint] = Set()
+  private def getEntryFunc(cp: ControlPoint): Set[String] = {
+    val rp = getRpOf(cp)
+    if (visited contains rp) Set()
+    else {
+      visited += rp
+      rp.func.algo.head match {
+        case head: SyntaxDirectedHead if head.withParams.isEmpty =>
+          Set(rp.func.name)
+        case _ => getCallNodes(rp).flatMap(getEntryFunc(_))
+      }
+    }
+  }
+  private def getRpOf(cp: ControlPoint): ReturnPoint = cp match {
+    case rp @ ReturnPoint(_, _) => rp
+    case NodePoint(node, view) =>
+      cfg.funcs.find(x => x.nodes.exists(x => x.uid == node.uid)) match {
+        case None => ???
+        case Some(f) => ReturnPoint(f, view)
+      }
+  }
+  private def getCallNodes(rp: ReturnPoint): Set[NodePoint[Call]] =
+    sem.getRetEdges(rp).map(_._1)
+
   // run repl
   def run(cp: ControlPoint): Unit = if (!continue || isBreak(cp)) runDirect(cp)
   def runDirect(givenCP: ControlPoint): Unit = {
@@ -158,32 +188,15 @@ object AnalyzeREPL {
         case CmdContinue.name :: _ =>
           continue = true; false
         case CmdBreak.name :: args =>
-          import CmdBreak._
-          args match {
-            case s"-${ FuncTarget.name }" :: bp => addBreak(FuncTarget, bp)
-            case s"-${ BlockTarget.name }" :: bp => addBreak(BlockTarget, bp)
-            case _ => println("Inappropriate option")
-          }; true
+          break(args); true
         case CmdListBreak.name :: _ =>
           breakpoints.zipWithIndex.foreach {
             case ((k, v), i) => println(f"$i: $k%-15s $v")
           }; true
         case CmdRmBreak.name :: args =>
-          args match {
-            case Nil => println("need arguments")
-            case arg :: _ => optional(arg.toInt) match {
-              case None =>
-                if (arg == s"-${CmdRmBreak.AllTarget.name}") breakpoints.clear
-                else println("Inappropriate argument")
-              case Some(idx) if idx.toInt < breakpoints.size => breakpoints.remove(idx.toInt)
-              case Some(idx) => println(s"out of index: $idx")
-            }
-          }; true
+          rmBreak(args); true
         case CmdLog.name :: _ =>
           Stat.dump(); true
-        case CmdGraph.name :: List("-total") =>
-          dumpCFG(cp, depth = None)
-          true
         case CmdGraph.name :: args =>
           graph(cp, args)
           true
@@ -191,12 +204,7 @@ object AnalyzeREPL {
         case CmdStop.name :: _ =>
           stop(); false
         case CmdInfo.name :: args =>
-          import CmdInfo._
-          args match {
-            case s"-${ RetTarget.name }" :: bp => printInfo(RetTarget, bp)
-            case s"-${ BlockTarget.name }" :: bp => printInfo(BlockTarget, bp)
-            case _ => println("Inappropriate option")
-          }; true
+          info(args); true
         case CmdEntry.name :: _ =>
           visited = Set()
           cp.map(getEntryFunc(_).foreach(println _))
@@ -221,10 +229,7 @@ object AnalyzeREPL {
 private abstract class Command(
   val name: String,
   val info: String = ""
-) { val options = List[CmdOption]() }
-
-// option
-private abstract class CmdOption(val name: String)
+) { val options = List[String]() }
 
 private object Command {
   val commands: List[Command] = List(
@@ -255,38 +260,32 @@ private case object CmdHelp extends Command("help")
 private case object CmdContinue extends Command("continue", "Continue the analysis.")
 
 private case object CmdBreak extends Command("break", "Add a break point.") {
-
-  case object FuncTarget extends CmdOption("func")
-
-  case object BlockTarget extends CmdOption("block")
-
-  override val options = List(FuncTarget, BlockTarget)
+  val func = "func"
+  val block = "block"
+  override val options = List(func, block)
 }
 
 private case object CmdListBreak extends Command("list-break", "Show the list of break points.")
 
 private case object CmdRmBreak extends Command("rm-break", "Remove a break point.") {
-
-  case object AllTarget extends CmdOption("all")
-
-  override val options = List(AllTarget)
+  val all = "all"
+  override val options = List(all)
 }
 
 private case object CmdLog extends Command("log", "Dump the state.")
 
-private case object CmdGraph extends Command("graph", "Dump the current control graph.")
+private case object CmdGraph extends Command("graph", "Dump the current control graph.") {
+  val total = "total"
+}
 
 private case object CmdExit extends Command("exit", "Exit the analysis.")
 
 private case object CmdStop extends Command("stop", "Stop the repl.")
 
 private case object CmdInfo extends Command("info", "Show abstract state of node") {
-
-  case object RetTarget extends CmdOption("ret")
-
-  case object BlockTarget extends CmdOption("block")
-
-  override val options = List(RetTarget, BlockTarget)
+  val ret = "ret"
+  val block = "block"
+  override val options = List(ret, block)
 }
 
 private case object CmdEntry extends Command("entry", "Show the set of entry functions of current function")
