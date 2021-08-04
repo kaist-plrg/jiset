5e14a7834b71c5576da3d2692c3cbff88ab84b2d
jhnaldo <jhpjhp0223@gmail.com>
2021-08-04 17:51:49 +0900

    Removed unnecessary checks in Obj

--- src/main/scala/kr/ac/kaist/jiset/ir/state/Obj.scala
+++ src/main/scala/kr/ac/kaist/jiset/ir/state/Obj.scala
@@ -5,11 +5,6 @@ import scala.collection.mutable.{ Map => MMap }
 
 // IR Objects
 sealed trait Obj extends IRComponent {
-  // TODO REMOVE
-  if (ty == Ty("Completion")) {
-    error("completion typed object is not supported")
-  }
-
   // types
   def ty: Ty
 
