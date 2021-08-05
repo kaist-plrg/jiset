ea8269e026bbbcc470b759bdd8e38a0ff63cda40
h2oche <h2oche22@gmail.com>
2021-08-05 18:00:15 +0900

    Implement CFG PartialModel construction

--- src/main/scala/kr/ac/kaist/jiset/checker/CFGPartialModel.scala
+++ src/main/scala/kr/ac/kaist/jiset/checker/CFGPartialModel.scala
@@ -1,9 +1,13 @@
 package kr.ac.kaist.jiset.checker
 
 import kr.ac.kaist.jiset.cfg._
+import kr.ac.kaist.jiset.util.Useful._
+import scala.annotation.tailrec
 
 // PartialModel
-case class CFGPartialModel(partialMap: Map[Function, Map[View, PartialFunc]]) {
+case class CFGPartialModel(
+  partialMap: Map[Function, Map[View, PartialFunc]]
+) extends CheckerElem {
   // getter
   def get(func: Function, view: View): Option[PartialFunc] =
     partialMap
@@ -11,22 +15,67 @@ case class CFGPartialModel(partialMap: Map[Function, Map[View, PartialFunc]]) {
       .get(view)
 }
 object CFGPartialModel {
-  def apply(recorder: VisitRecorder): CFGPartialModel = CFGPartialModel(recorder.visitMap.map {
-    case (func, viewMap) => func -> (viewMap.map {
-      case (view, nodeMap) => view -> PartialFunc(func, view, nodeMap.keys.toSet)
-    }).toMap
-  }.toMap)
+  def apply(recorder: VisitRecorder): CFGPartialModel = CFGPartialModel(
+    recorder.visitMap.map {
+      case (func, viewMap) => func -> (viewMap.map {
+        case (view, nodeMap) => view -> PartialFunc(func, view, nodeMap.keys.toSet)
+      }).toMap
+    }.toMap
+  )
 }
 
 // PartialFunc for each function and view
-case class PartialFunc(func: Function, view: View, reachables: Set[Node]) extends CFGElem {
+case class PartialFunc(
+  func: Function,
+  view: View,
+  reachables: Set[Node]
+) extends CheckerElem {
   import PartialFunc._
 
-  // Get the information of given branch
-  lazy val reachable: Map[Branch, Reachable] = ???
+  // get reachable status
+  private def getReachable(t: Node, e: Node): Reachable =
+    (reachables contains t, reachables contains e) match {
+      case (true, true) => Reachable.Both
+      case (true, false) => Reachable.Then
+      case (false, true) => Reachable.Else
+      case (false, false) => error(s"Should be reachable branch")
+    }
 
-  // Get the shortcut
-  lazy val shortcut: Map[Linear, Node] = ???
+  // jump branch node
+  @tailrec
+  private def jump(branch: Branch): Node = {
+    val (t, e) = func.branches(branch)
+    reachable(branch) match {
+      case Reachable.Both => branch
+      case Reachable.Then => t match {
+        case b: Branch => jump(b)
+        case _ => t
+      }
+      case Reachable.Else => e match {
+        case b: Branch => jump(b)
+        case _ => e
+      }
+    }
+  }
+
+  // get the information of given branch
+  lazy val reachable: Map[Branch, Reachable] = (for {
+    branch <- reachables.collect { case b: Branch => b }
+    (t, e) <- func.branches.get(branch)
+  } yield branch -> getReachable(t, e)).toMap
+
+  // shortcut of linear node
+  lazy val shortcut: Map[Linear, Node] = (for {
+    linear <- reachables.collect { case l: Linear => l }
+    nextNode <- func.nexts.get(linear)
+    dest <- nextNode match {
+      case b: Branch => reachable(b) match {
+        case Reachable.Both => None
+        case _ => Some(jump(b))
+      }
+      case _ => None
+    }
+  } yield linear -> dest).toMap
 
   // order between partial model
   def ⊑(that: PartialFunc): Boolean = this.reachables subsetOf that.reachables
