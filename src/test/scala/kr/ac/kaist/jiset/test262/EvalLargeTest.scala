8fa852d7f42983b031747e238ade9732ceb11590
jaeho <jaeho@plrg2.kaist.ac.kr>
2021-09-01 13:00:09 +0900

    Added bugtrigger option in test262EvalTest

--- src/test/scala/kr/ac/kaist/jiset/test262/EvalLargeTest.scala
+++ src/test/scala/kr/ac/kaist/jiset/test262/EvalLargeTest.scala
@@ -2,6 +2,8 @@ package kr.ac.kaist.jiset.test262
 
 import kr.ac.kaist.jiset._
 import kr.ac.kaist.jiset.js.Test262
+import kr.ac.kaist.jiset.util.JvmUseful._
+import kr.ac.kaist.jiset.util.Useful._
 
 class EvalLargeTest extends Test262Test {
   val name: String = "test262EvalTest"
@@ -9,6 +11,13 @@ class EvalLargeTest extends Test262Test {
   // logging with view information
   LOG = true
   VIEW = true
+  BUGTRIGGER = optional(readFile(s"$BASE_DIR/tests/.bugtrigger").trim)
+  BUGTRIGGER match {
+    case Some(str) if exists(s"$VERSION_DIR/bugtrigger/$str/algo") =>
+      println(s"'$str' is successfully triggered.")
+    case Some(str) => println(s"'$str' is an invalid bug name. No bugs are triggered.")
+    case None => println("Since the .bugtrigger file does not exist, no bugs are triggered.")
+  }
 
   // registration
   def init: Unit = check(name, {
