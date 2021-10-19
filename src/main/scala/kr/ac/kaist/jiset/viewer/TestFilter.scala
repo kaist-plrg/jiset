7d3bac8b4dbf79b1c751a67b92245df10047d253
h2oche <h2oche22@gmail.com>
2021-10-19 15:53:13 +0900

    Dump various stats related to test262 trace

--- src/main/scala/kr/ac/kaist/jiset/viewer/TestFilter.scala
+++ src/main/scala/kr/ac/kaist/jiset/viewer/TestFilter.scala
@@ -39,26 +39,32 @@ object JSFilter {
   // TestList
   case class TestList(tests: List[Test], nodeMap: Map[Node, List[Test]]) {
     // inverse document frequency(idf)
-    def n_t(node: Node) = tests.filter(_.trace.contains(node)).size + 1
+    def n_t(node: Node) = nodeMap.getOrElse(node, List()).size + 1
     def idf_basic(node: Node) = log10(tests.size / n_t(node))
-    def idf_smooth(node: Node) = log10(tests.size / (1 + n_t(node))) + 1
+    def idf_smooth(node: Node) = log10(tests.size / n_t(node)) + 1
     def idf_prob(node: Node) = log10((tests.size - n_t(node)) / n_t(node))
     def tfidf(node: Node, test: Test) = test.tf_raw(node) * idf_smooth(node)
 
     // sorted by tf-idf score
-    lazy val sortedNodeMap: Map[Node, List[Test]] = (for {
-      (node, tests) <- nodeMap
-      // sort test by tfidf score
-      sorted = tests.sortWith {
-        case (t0, t1) => tfidf(node, t1) < tfidf(node, t0)
-      }
-    } yield node -> sorted).toMap
+    // lazy val sortedNodeMap: Map[Node, List[Test]] = (for {
+    //   (node, tests) <- nodeMap
+    //   // sort test by tfidf score
+    //   sorted = tests.sortWith {
+    //     case (t0, t1) => tfidf(node, t1) < tfidf(node, t0)
+    //   }
+    // } yield node -> sorted).toMap
+
+    // dump
+    def dump(dirname: String = BASE_DIR): Unit = {
+      mkdir(dirname)
+      dumpScore(dirname)
+      dumpTestTraceSize(dirname)
+      dumpNodeTouchedSize(dirname)
+    }
 
     // dump tf-idf score
     def dumpScore(dirname: String = BASE_DIR): Unit = {
-      mkdir(dirname)
       val nf = getPrintWriter(s"$dirname/tfidf.csv")
-      println("!!! dump start")
       nf.println(s",${nodeMap.map(_._1.uid).mkString(",")}")
       tests.foreach { test =>
         val scores = nodeMap.map {
@@ -67,13 +73,35 @@ object JSFilter {
             else .0
         }
         nf.println(s"${test.name},${scores.mkString(",")}")
-        println(test.name)
       }
       nf.close()
     }
+
+    // dump trace size of each test
+    def dumpTestTraceSize(dirname: String = BASE_DIR): Unit = {
+      val nf = getPrintWriter(s"$dirname/test_trace_size.csv")
+      tests.foreach { test =>
+        nf.println(s"${test.name},${test.trace.size}")
+      }
+      nf.close()
+    }
+
+    // dump touched test size of each node in cfg
+    def dumpNodeTouchedSize(dirname: String = BASE_DIR): Unit = {
+      val nf = getPrintWriter(s"$dirname/node_touched_size.tsv")
+      cfg.nodes.sortWith(_.uid < _.uid).foreach { node =>
+        {
+          val names = nodeMap.getOrElse(node, List()).map(_.name)
+          nf.println(s"${node.uid}\t${cfg.funcOf(node).name}\t${names.size}")
+        }
+      }
+      nf.close()
+    }
+
+    override def toString: String = ???
   }
   object TestList {
-    def apply(dirname: String = VISITED_LOG_DIR): TestList = {
+    def apply(dirname: String = VISITED_LOG_DIR): Unit = {
       var tests: List[Test] = List()
       var nodeMap: Map[Node, List[Test]] = Map()
       // walk visited-nodes directory
@@ -88,13 +116,8 @@ object JSFilter {
         }
         // update total test list
         tests ::= test
-        println(test.name)
       }
-      println("!!! initialized")
-      val tl = TestList(tests, nodeMap) // TODO sort tests
-      println("!!! create test list obj")
-      tl.dumpScore()
-      tl
+      TestList(tests, nodeMap) // TODO sort tests
     }
   }
 }
