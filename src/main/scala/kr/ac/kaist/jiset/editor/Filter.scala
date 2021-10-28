8d906e2f37c87fa9906b05d02c71cce1cf39f5e9
jaeho <jaeho@plrg2.kaist.ac.kr>
2021-10-28 20:04:32 +0900

    Added touched map

--- src/main/scala/kr/ac/kaist/jiset/editor/Filter.scala
+++ src/main/scala/kr/ac/kaist/jiset/editor/Filter.scala
@@ -21,6 +21,10 @@ object Filter {
   // mapping from node to shortest program
   var nodeMap: NodeMap = Map()
 
+  // mapping from uid of program to touched nodes
+  type Flag = Array[Boolean]
+  var touchedMap: Map[String, Flag] = Map()
+
   // count put operation
   var putCount = 0
 
@@ -32,6 +36,12 @@ object Filter {
   def put(p: JsProgram): Unit = {
     putCount += 1
 
+    // uid of the program
+    val pid = p.uid
+
+    // create touched flag
+    var flag = touchedMap.getOrElse(pid, Array.fill(cfg.nodes.size)(false))
+
     // create interp object
     val interp = new Interp(p.initState, useHook = true)
 
@@ -40,26 +50,34 @@ object Filter {
     interp.subscribe(Interp.Event.Step, { st =>
       val cursor = st.context.cursorOpt.get
       cursor match {
-        case NodeCursor(n) => nodeMap.get(n) match {
-          case Some(p0) if p0.size <= p.size =>
-          case _ =>
-            nodeMap += (n -> p) // TODO save touched
+        case NodeCursor(n) =>
+          //flag update
+          flag(n.uid) = true
 
-            // logging
-            if (LOG) {
-              if (!printed) {
-                nfLog.println("!!!", p.toString)
-                printed = true
+          //nodeMap update
+          nodeMap.get(n) match {
+            case Some(p0) if p0.size <= p.size =>
+            case _ =>
+              nodeMap += (n -> p) // TODO save touched
+
+              // logging
+              if (LOG) {
+                if (!printed) {
+                  nfLog.println("!!!", p.toString)
+                  printed = true
+                }
+                nfLog.println(s"$putCount, ${n.uid}, ${nodeMap.size}")
               }
-              nfLog.println(s"$putCount, ${n.uid}, ${nodeMap.size}")
-            }
-        }
+          }
         case _ =>
       }
     })
 
     // fixpoint
     interp.fixpoint
+
+    //touchedMap update
+    touchedMap += (p.uid -> flag)
   }
 
   // select one program and try to reduce its size
@@ -86,6 +104,25 @@ object Filter {
     putCount = c
     nodeMap = m
   }
+
+  // ??
+  //  def touchPrograms(nid: Int): Set[String] = (for {
+  //    (pid, flag) <- touchedMap
+  //    if flag(nid)
+  //  } yield pid).toSet
+  //
+  //  def dumpPrograms: Unit = for {
+  //    (_, nid) <- Array.fill(cfg.nodes.size)(0).zipWithIndex
+  //  } {
+  //    val nf = getPrintWriter(s"$EDITOR_LOG_DIR/$nid.log")
+  //    val ps = touchPrograms(nid)
+  //    for {
+  //      p <- ps
+  //    } {
+  //      nf.println(s"$p")
+  //    }
+  //    nf.close
+  //  }
 
   // close
   def close(): Unit = nfLog.close()
