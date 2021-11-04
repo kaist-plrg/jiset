3d31209330830cf65f13f9c1177595e42fb49144
jaeho <jaeho@plrg2.kaist.ac.kr>
2021-11-04 12:05:26 +0900

    In-depth transformation

--- src/main/scala/kr/ac/kaist/jiset/editor/mutator/RandomMutator.scala
+++ src/main/scala/kr/ac/kaist/jiset/editor/mutator/RandomMutator.scala
@@ -2,19 +2,30 @@ package kr.ac.kaist.jiset.editor
 
 import kr.ac.kaist.jiset._
 import kr.ac.kaist.jiset.js._
-import kr.ac.kaist.jiset.js.ast.Script
+import kr.ac.kaist.jiset.js.ast.{ Script, StatementList }
 import kr.ac.kaist.jiset.util._
 import kr.ac.kaist.jiset.util.JvmUseful._
 import kr.ac.kaist.jiset.util.Useful._
 
 // random mutator
-case class RandomMutator(program: JsProgram, nids: Set[Int]) extends Mutator {
+case class RandomMutator(program: JsProgram, nids: Set[Int]) extends Mutator with ASTTransformer {
+  // handle stmts
+  var transformed :Boolean = false
+  override def transform(ast: StatementList): StatementList = if (!transformed) {
+    val stmts = flattenStmtList(ast)
+    val length = stmts.length
+    if (length > 1) {
+      val sep = randInt(length)
+      val removed = stmts.take(sep) ++ stmts.takeRight(stmts.size - sep - 1)
+      transformed = true
+      mergeStmtList(removed, ast.parserParams).get
+    } else ast
+  } else ast
+
   val name: String = "random"
   val retryMax = 10
   def _mutate: Option[Script] = {
-    val stmts = flattenStmt(program.script)
-    val sep = randInt(stmts.length)
-    val removed = stmts.take(sep) ++ stmts.takeRight(stmts.size - sep - 1)
-    Some(mergeStmt(removed))
+   val removed = transform(program.script)
+   Some(removed)
   }
 }
