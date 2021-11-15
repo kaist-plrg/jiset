61454164752f075ad188e65d81ad1491dbc07c50
h2oche <h2oche22@gmail.com>
2021-11-15 17:41:23 +0900

    Revert collect phase

--- src/main/scala/kr/ac/kaist/jiset/phase/Collect.scala
+++ src/main/scala/kr/ac/kaist/jiset/phase/Collect.scala
@@ -22,11 +22,36 @@ case object Collect extends Phase[Unit, CollectConfig, Unit] {
     jisetConfig: JISETConfig,
     config: CollectConfig
   ): Unit = {
-    val filename = getFirstFilename(jisetConfig, "parse")
     setSpec(loadSpec(s"$VERSION_DIR/generated"))
-    val ast = parseFile(filename)
-    val collector = Collector(ast)
-    dumpFile(collector.toJson, s"$BASE_DIR/collect.json")
+    mkdir(s"$LOG_DIR/collect")
+
+    // read test262 list
+    val tests = readFile(s"$BASE_DIR/tests/analyze-test262").split(LINE_SEP).toList
+    val targets = Test262.config.normal.filter(tests contains _.name)
+
+    // collect
+    ProgressBar("collect", targets.zipWithIndex).foreach(config => {
+      // parse test262
+      val (NormalTestConfig(name, includes), idx) = config
+      val jsName = s"$TEST262_TEST_DIR/$name"
+      val includeStmts = includes.foldLeft(basicStmts) {
+        case (li, s) => for {
+          x <- li
+          y <- getInclude(s)
+        } yield x ++ y
+      } match {
+        case Right(l) => l
+        case Left(msg) => throw NotSupported(msg)
+      }
+      val stmts = includeStmts ++ flattenStmt(parseFile(jsName))
+      val merged = mergeStmt(stmts)
+
+      // collect js states
+      val collector = Collector(merged)
+
+      // dump js states
+      dumpFile(collector.toJson, s"$LOG_DIR/collect/$idx.json")
+    })
   }
 
   def defaultConfig: CollectConfig = CollectConfig()
