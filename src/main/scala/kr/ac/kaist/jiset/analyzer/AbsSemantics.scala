6671209f65d8dd47513cc83c46e87bd4463f11cd
h2oche <h2oche22@gmail.com>
2021-11-19 18:32:22 +0900

    Revert "Adjust sensitivity"
    This reverts commit ba4f7180d5b6a89ccdaa510533fdb2b7bba1cfc5.

--- src/main/scala/kr/ac/kaist/jiset/analyzer/AbsSemantics.scala
+++ src/main/scala/kr/ac/kaist/jiset/analyzer/AbsSemantics.scala
@@ -180,10 +180,7 @@ case class AbsSemantics(
     astOpt match {
       // flow sensitivity
       case Some(ast) =>
-        val boundedLoops = loops.map {
-          case LoopCtxt(loop, k) => LoopCtxt(loop, handleSens(k, LOOP_ITER))
-        }
-        val newJsLoops = handleSens(boundedLoops ++ jsLoops, LOOP_DEPTH)
+        val newJsLoops = handleSens(loops ++ jsLoops, LOOP_DEPTH)
         View(Some(JSView(ast, jsCalls, newJsLoops)), Nil, Nil, 0)
 
       // call-site sensitivity
@@ -217,12 +214,12 @@ case class AbsSemantics(
   // loop transition
   def loopNext(view: View): View = view.loops match {
     case LoopCtxt(loop, k) :: rest =>
-      view.copy(loops = LoopCtxt(loop, k + 1) :: rest)
+      view.copy(loops = LoopCtxt(loop, handleSens(k + 1, LOOP_ITER)) :: rest)
     case _ => view
   }
   def loopEnter(view: View, loop: Loop): View = {
     val loopView = view.copy(
-      loops = LoopCtxt(loop, 0) :: view.loops,
+      loops = handleSens(LoopCtxt(loop, 0) :: view.loops, LOOP_DEPTH),
       intraLoopDepth = view.intraLoopDepth + 1,
     )
     loopOut += loopView -> (loopOut.getOrElse(loopView, Set()) + view)
